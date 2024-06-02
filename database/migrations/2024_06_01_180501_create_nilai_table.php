9030df57b14f064d91c45d75b2e1848ca0ac704b
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2024-06-02 22:34:58 +0700

    Update 2024_06_01_180501_create_nilai_table.php

--- database/migrations/2024_06_01_180501_create_nilai_table.php
+++ database/migrations/2024_06_01_180501_create_nilai_table.php
@@ -4,27 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateNilaisTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
-        Schema::create('nilai', function (Blueprint $table) {
+        Schema::create('nilais', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('kandidat_id')->constrained('kandidats');
-            $table->foreignId('kriteria_id')->constrained('kriterias');
-            $table->float('nilai');
+            $table->foreignId('kandidat_id')->constrained()->onDelete('cascade');
+            $table->string('kriteria');
+            $table->integer('nilai');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('nilai');
+        Schema::dropIfExists('nilais');
     }
-};
+}
