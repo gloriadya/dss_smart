34908a597a7a32730dbb82dcd5e90ec2566bbba8
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2024-06-02 22:37:20 +0700

    Update index.blade.php

--- resources/views/kandidats/index.blade.php
+++ resources/views/kandidats/index.blade.php
@@ -1,25 +1,45 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Penilaian Kandidat</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
 <div class="container">
-    <a href="{{ route('kandidats.create') }}" class="btn btn-primary">Tambah Kandidat</a>
-    <table class="table mt-4">
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($kandidats as $kandidat)
-            <tr>
-                <td>{{ $kandidat->nama }}</td>
-                <td>
-                    <a href="#" class="btn btn-info">Detail</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <h1 class="mt-5">Penilaian Kandidat</h1>
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+    <form method="POST" action="{{ route('nilai.store') }}">
+        @csrf
+        <div class="form-group">
+            <label for="kandidat_id">Nama Kandidat</label>
+            <select class="form-control" id="kandidat_id" name="kandidat_id">
+                @foreach($kandidats as $kandidat)
+                    <option value="{{ $kandidat->id }}">{{ $kandidat->nama }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="kriteria">Kriteria</label>
+            <select class="form-control" id="kriteria" name="kriteria">
+                <option value="Pengalaman Kerja">Pengalaman Kerja</option>
+                <option value="Pendidikan">Pendidikan</option>
+                <option value="Kepribadian dan Keterampilan">Kepribadian dan Keterampilan</option>
+                <option value="Referensi">Referensi</option>
+                <option value="Tes Keterampilan">Tes Keterampilan</option>
+                <option value="Keterampilan">Keterampilan</option>
+                <option value="Keahlian Teknis">Keahlian Teknis</option>
+                <option value="Kesesuaian Budaya Perusahaan">Kesesuaian Budaya Perusahaan</option>
+                <option value="Wawancara">Wawancara</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="nilai">Nilai</label>
+            <input type="number" class="form-control" id="nilai" name="nilai" min="1" max="100" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
-@endsection
+</body>
+</html>
