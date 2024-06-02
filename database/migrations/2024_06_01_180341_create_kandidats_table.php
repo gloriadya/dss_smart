f5957a594506d6cf65c8e1991f82ad75a5c94400
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2024-06-02 22:34:38 +0700

    Update 2024_06_01_180341_create_kandidats_table.php

--- database/migrations/2024_06_01_180341_create_kandidats_table.php
+++ database/migrations/2024_06_01_180341_create_kandidats_table.php
@@ -4,26 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateKandidatsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    
     public function up()
     {
         Schema::create('kandidats', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->text('alamat');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('kandidats');
     }
-};
+}
