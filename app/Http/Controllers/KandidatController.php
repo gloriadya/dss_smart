7d0e34fec19fba721e97b9fcfa688de2dd242923
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2024-06-02 22:33:23 +0700

    Update KandidatController.php

--- app/Http/Controllers/KandidatController.php
+++ app/Http/Controllers/KandidatController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kandidat;
-use App\Models\Kriteria;
 use App\Models\Nilai;
 use Illuminate\Http\Request;
 
@@ -11,32 +10,51 @@ class KandidatController extends Controller
 {
     public function index()
     {
-        return response()->json(Kandidat::all());
+        return view('kandidat.index', ['kandidats' => Kandidat::all()]);
     }
 
     public function store(Request $request)
     {
-        $kandidat = Kandidat::create($request->all());
-        return response()->json($kandidat, 201);
+        $validatedData = $request->validate([
+            'kandidat_id' => 'required|exists:kandidats,id',
+            'kriteria' => 'required|string',
+            'nilai' => 'required|integer|min:1|max:100',
+        ]);
+
+        $nilai = new Nilai();
+        $nilai->kandidat_id = $validatedData['kandidat_id'];
+        $nilai->kriteria = $validatedData['kriteria'];
+        $nilai->nilai = $validatedData['nilai'];
+        $nilai->save();
+
+        return redirect()->back()->with('success', 'Nilai berhasil disimpan.');
     }
 
     public function calculate()
     {
-        $kandidats = Kandidat::with('nilai.kriteria')->get();
-        $kriterias = Kriteria::all();
-
-        $totalBobot = $kriterias->sum('bobot');
-        $kriterias->each(function ($item) use ($totalBobot) {
-            $item->bobot_normalized = $item->bobot / $totalBobot;
-        });
+        $kandidats = Kandidat::with('nilai')->get();
+        $kriteriaBobot = [
+            'Pengalaman Kerja' => 0.125,
+            'Pendidikan' => 0.0833,
+            'Kepribadian dan Keterampilan' => 0.1667,
+            'Referensi' => 0.0833,
+            'Tes Keterampilan' => 0.0417,
+            'Keterampilan' => 0.0833,
+            'Keahlian Teknis' => 0.0833,
+            'Kesesuaian Budaya Perusahaan' => 0.0833,
+            'Wawancara' => 0.25,
+        ];
 
         foreach ($kandidats as $kandidat) {
-            $kandidat->total_score = 0;
+            $totalScore = 0;
             foreach ($kandidat->nilai as $nilai) {
-                $kandidat->total_score += $nilai->nilai * $nilai->kriteria->bobot_normalized;
+                $totalScore += $nilai->nilai * $kriteriaBobot[$nilai->kriteria];
             }
+            $kandidat->total_score = $totalScore;
         }
 
-        return response()->json($kandidats);
+        $sortedKandidats = $kandidats->sortByDesc('total_score');
+
+        return view('hasil_perankingan', ['kandidats' => $sortedKandidats]);
     }
 }
